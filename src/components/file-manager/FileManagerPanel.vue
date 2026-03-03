a5b801df721897084a204b75ae00543ad16f5f51
ixpl0 <kykymapua+github@gmail.com>
2026-03-03 04:32:07 +0400

    Hide Delete option for git-deleted files in context menu

--- src/components/file-manager/FileManagerPanel.vue
+++ src/components/file-manager/FileManagerPanel.vue
@@ -66,6 +66,7 @@
           Revert changes
         </button>
         <button
+          v-if="contextMenu.status !== 'deleted'"
           type="button"
           class="btn btn-ghost btn-sm w-full justify-start text-error"
           :disabled="isActionInProgress"
